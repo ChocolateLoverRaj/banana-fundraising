a00ce25a5b7945dfde77bb2695045630b39fa760
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2021-05-12 07:44:33 -0500

    Menu Router

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,22 +2,38 @@ import { Menu } from "antd";
 import Image from "next/image";
 import Icon from "@ant-design/icons";
 import styles from "./Header.module.css";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+
+  const handleSelect = ({ key }) => {
+    if (key !== "/logo") router.replace(key);
+  };
+
   return (
-    <Menu mode="horizontal">
+    <Menu
+      mode="horizontal"
+      selectedKeys={[router.route]}
+      onSelect={handleSelect}
+    >
       <Menu.Item
-        key="logo"
+        key="/logo"
         icon={
           <Icon
             component={() => <Image src="/logo.png" height={14} width={14} />}
           />
         }
-      />
-      <Menu.Item key="name">Banana Fundraising</Menu.Item>
-      <Menu.Item key="space" className={styles.space} />
-      <Menu.Item key="students">Students</Menu.Item>
-      <Menu.Item key="teachers">Teachers</Menu.Item>
+      >
+        <a
+          href="https://sites.google.com/pewaukeeschools.org/banana-custom-apps/home"
+          target="_blank"
+        />
+      </Menu.Item>
+      <Menu.Item key="/">Banana Fundraising</Menu.Item>
+      <Menu.Item key="/space" className={styles.space} />
+      <Menu.Item key="/students">Students</Menu.Item>
+      <Menu.Item key="/teachers">Teachers</Menu.Item>
     </Menu>
   );
 };
