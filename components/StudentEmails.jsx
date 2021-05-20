011d87d0f468d73a8c5ce88e5719f3ca6a1a3f91
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2021-05-20 12:18:17 -0500

    new email

--- components/StudentEmails.jsx
+++ components/StudentEmails.jsx
@@ -1,15 +1,39 @@
-import { Row, Col, Button } from "antd";
+import { Row, Col, Button, Modal, Form, Input } from "antd";
+import { useState } from "react";
 
 const StudentEmails = (props) => {
+  const [creating, setCreating] = useState(false);
+
+  const handleNewEmail = () => {
+    setCreating(true);
+  };
+
   return (
-    <Row justify="space-between">
-      <Col>
-        <h2>Emails</h2>
-      </Col>
-      <Col>
-        <Button>New Email</Button>
-      </Col>
-    </Row>
+    <>
+      <Row justify="space-between">
+        <Col>
+          <h2>Emails</h2>
+        </Col>
+        <Col>
+          <Button onClick={handleNewEmail}>New Email</Button>
+        </Col>
+      </Row>
+      <Modal visible={creating} title="New Email" okText="Send">
+        <Form>
+          <Form.Input name="to" label="To" rules={[{ type: "email" }]}>
+            <Input />
+          </Form.Input>
+          <Form.Input
+            name="subject"
+            label="Subject"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Input>
+          <Form.Input name="body">H</Form.Input>
+        </Form>
+      </Modal>
+    </>
   );
 };
 
