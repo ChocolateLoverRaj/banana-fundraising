352db9dff825f337f4de89c1113a0d6416218a30
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2021-05-26 08:05:02 -0500

    not not bordered

--- pages/email.jsx
+++ pages/email.jsx
@@ -1,18 +1,26 @@
-import { useRouter } from 'next/router'
-import { Input, Alert } from 'antd'
-import * as sanitize from 'sanitize-html'
+import { useRouter } from "next/router";
+import { Input, Alert } from "antd";
+import * as sanitize from "sanitize-html";
 
 const EmailPage = () => {
-  const { query: { from, subject, to, body } } = useRouter()
+  const {
+    query: { from, subject, to, body }
+  } = useRouter();
 
   return (
     <>
-      <Alert message={<>Viewing demo email of <b>{to}</b></>} />
-      <Input addonBefore='From' defaultValue={from} readOnly bordered={false} />
-      <Input addonBefore='Subject' defaultValue={subject} readOnly bordered={false} />
-      <div dangerouslySetInnerHTML={{__html: sanitize(body)}} />
+      <Alert
+        message={
+          <>
+            Viewing demo email of <b>{to}</b>
+          </>
+        }
+      />
+      <Input addonBefore="From" defaultValue={from} readOnly />
+      <Input addonBefore="Subject" defaultValue={subject} readOnly />
+      <div dangerouslySetInnerHTML={{ __html: sanitize(body) }} />
     </>
-  )
-}
+  );
+};
 
-export default EmailPage
+export default EmailPage;
