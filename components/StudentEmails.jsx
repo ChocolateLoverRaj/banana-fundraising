c5d55a6eefbc38b38473cc61d0f4312426e2ae38
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2021-05-26 08:32:38 -0500

    stuff

--- components/StudentEmails.jsx
+++ components/StudentEmails.jsx
@@ -1,9 +1,21 @@
-import { Row, Col, Button, Modal, Form, Input, Alert, message } from "antd";
+import {
+  Row,
+  Col,
+  Button,
+  Modal,
+  Form,
+  Input,
+  Alert,
+  notification
+} from "antd";
 import { useState, useEffect, useContext } from "react";
 import { MdInput } from ".";
 import { GlobalContext } from "../helpers";
+import mdToHtml from "marked";
+import * as sanitize from "sanitize-html";
+import Link from "next/link";
 
-const StudentEmails = (props) => {
+const StudentEmails = () => {
   const [creating, setCreating] = useState(false);
   const [createReq, setCreateReq] = useState();
   const [form] = Form.useForm();
@@ -12,19 +24,45 @@ const StudentEmails = (props) => {
   useEffect(() => {
     /* eslint-disable-next-line */
     createReq
-      ?.then((res) => {
+      ?.then(async (res) => {
         if (res.status === 201) {
           setCreating(false);
-          message.success("Email Sent");
+          const {
+            to: { email: to },
+            subject,
+            message
+          } = form.getFieldsValue();
+          const emailId = await res.json();
+          notification.success({
+            message: "Email Sent",
+            description: (
+              <>
+                Click{" "}
+                <Link
+                  href={`/email?to=${to}&subject=${subject}&body=${sanitize(
+                    mdToHtml(message)
+                  )}<br><a href="${
+                    window.location.origin
+                  }/donate?emailId=${emailId}">Click Here To Donate</a>&from=${
+                    login?.email
+                  }`}
+                >
+                  here
+                </Link>{" "}
+                to view email.
+              </>
+            )
+          });
         } else {
           alert("Error sending email");
         }
         setCreateReq(undefined);
       })
-      .catch(() => {
+      .catch((e) => {
+        console.log(e);
         alert("error creating email");
       });
-  }, [createReq]);
+  }, [createReq, form, login]);
 
   const handleNewEmail = () => {
     setCreating(true);
@@ -70,7 +108,7 @@ const StudentEmails = (props) => {
         onOk={handleOk}
       >
         <Alert
-          message="Emails will be sent from `testing@sparkpostbox.com`"
+          message="Emails will not actually be sent"
           description={
             <>
               The real website would use the{" "}
@@ -81,11 +119,8 @@ const StudentEmails = (props) => {
               >
                 Gmail API
               </a>
-              , but since the school has disabled it this website uses{" "}
-              <a target="_blank" href="https://sparkpost.com">
-                Sparkpost
-              </a>{" "}
-              to send emails.
+              , but since the school has disabled it this website doesn't
+              actually send emails.
             </>
           }
         />
--- pages/api/emails.js
+++ pages/api/emails.js
@@ -1,92 +1,76 @@
-import getRole from '../../helpers/getRole'
-import Ajv from 'ajv'
-import getDb from '../../helpers/db'
-import getId from '../../helpers/getId'
-import randomInt from '../../helpers/randomInt'
-import sparkPost from '../../helpers/sparkPost'
-import mdToHtml from 'marked'
-import * as sanitize from 'sanitize-html'
-import domain from '../../helpers/domain'
+import getRole from "../../helpers/getRole";
+import Ajv from "ajv";
+import getDb from "../../helpers/db";
+import getId from "../../helpers/getId";
+import randomInt from "../../helpers/randomInt";
 
 const schema = {
-  type: 'object',
+  type: "object",
   properties: {
     to: {
-      type: 'object',
+      type: "object",
       properties: {
         name: {
-          type: 'string',
+          type: "string",
           minLength: 1
         },
         email: {
-          type: 'string',
+          type: "string",
           minLength: 1
         }
       },
-      required: ['name', 'email']
+      required: ["name", "email"]
     },
     subject: {
-      type: 'string',
+      type: "string",
       minLength: 1
     },
     message: {
-      type: 'string',
+      type: "string",
       minLength: 1
     }
   },
-  required: ['to', 'subject', 'message']
-}
+  required: ["to", "subject", "message"]
+};
 
 export default async (req, res) => {
-  if (req.method !== 'POST') {
-    res.status(405).end()
-    return
+  if (req.method !== "POST") {
+    res.status(405).end();
+    return;
   }
-  const email = req.headers.token.toLowerCase()
-  const role = getRole(email)
-  if (role === 'none') {
-    res.status(403).end()
-    return
+  const email = req.headers.token.toLowerCase();
+  const role = getRole(email);
+  if (role === "none") {
+    res.status(403).end();
+    return;
   }
-  const ajv = new Ajv()
-  const valid = ajv.validate(schema, req.body)
+  const ajv = new Ajv();
+  const valid = ajv.validate(schema, req.body);
   if (!valid) {
-    console.log(req.body)
-    console.log(ajv.errors)
-    res.status(400).json(ajv.errors)
-    return
+    console.log(req.body);
+    console.log(ajv.errors);
+    res.status(400).json(ajv.errors);
+    return;
   }
-  const { db } = await getDb()
-  const schools = db.collection('schools')
-  const id = await getId()
-  const school = await schools.findOne({ id })
+  const { db } = await getDb();
+  const schools = db.collection("schools");
+  const id = await getId();
+  const school = await schools.findOne({ id });
   if (!school) {
-    res.status(403).end()
-    return
+    res.status(403).end();
+    return;
   }
-  const emailId = randomInt()
-  const studentIndex = school.students.findIndex(student => student.email === email)
-  await sparkPost.transmissions.send({
-    options: {
-      sandbox: true
-    },
-    recipients: [{
-      name: req.body.to.name,
-      address: req.body.to.email
-    }],
-    content: {
-      from: {
-        name: 'Test Student',
-        email: 'testing@sparkpostbox.com'
-      },
-      subject: req.body.subject,
-      html: `${sanitize(mdToHtml(req.body.message))}<br><a href="${domain}/donate?emailId=${emailId}">Click Here To Donate</a>`
+  const emailId = randomInt();
+  const studentIndex = school.students.findIndex(
+    (student) => student.email === email
+  );
+  await schools.updateOne(
+    { _id: school._id },
+    {
+      $push: {
+        [`students.${studentIndex}.emails`]: { id: emailId }
+      }
     }
-  })
-  await schools.updateOne({ _id: school._id }, {
-    $push: {
-      [`students.${studentIndex}.emails`]: { id: emailId }
-    }
-  })
-  res.status(201).end()
-}
+  );
+  res.status(201).json(emailId);
+};
